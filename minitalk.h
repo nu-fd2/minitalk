33f88d4d7dd579e08f21feed56c4b6a5fddc67de
fd2 <nu-fd2@users.noreply.github.com>
2025-02-01 17:14:30 +0100

    qwertyuiop

---
+++ minitalk.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oel-mado <oel-mado@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/01 16:54:55 by oel-mado          #+#    #+#             */
+/*   Updated: 2025/02/01 17:02:42 by oel-mado         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+#define MINITALK_H
+
+#include "ft_printf/src/ft_printf.h"
+#include "libft/libft.h"
+#include <signal.h>
+
+#endif //MINITALK_H
