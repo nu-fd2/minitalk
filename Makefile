068315552a2c2004dbd2b1227863163d0ff5ad5b
fd2 <nu-fd2@users.noreply.github.com>
2025-03-12 19:58:42 +0000

    waaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- Makefile
+++ Makefile
@@ -6,26 +6,31 @@
 #    By: oel-mado <oel-mado@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/02/01 16:55:57 by oel-mado          #+#    #+#              #
-#    Updated: 2025/02/04 19:42:50 by oel-mado         ###   ########.fr        #
+#    Updated: 2025/03/12 17:12:40 by oel-mado         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-
+PF = ft_printf/libftprintf.a
+LB = libft/libft.a
 
 msg:
 	@echo "do make s or make c"
 
-pf:
+$(PF):
 	make -C ft_printf re
+	make -C ft_printf clean
 
-lb:
+$(LB):
 	make -C libft re
+	make -C libft clean
 
-s: pf
-	cc server.c ft_printf/libftprintf.a -o server
+s: $(LB) $(PF)
+	cc server.c $(LB) $(PF) -o server
 
-c: lb pf
-	cc client.c libft/libft.a ft_printf/libftprintf.a -o client
+c: $(LB) $(PF)
+	cc client.c $(LB) $(PF) -o client
+
+cs:fclean c s
 
 clean:
 	make -C ft_printf clean
--- client.c
+++ client.c
@@ -6,62 +6,51 @@
 /*   By: oel-mado <oel-mado@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 15:17:06 by oel-mado          #+#    #+#             */
-/*   Updated: 2025/02/04 22:22:23 by oel-mado         ###   ########.fr       */
+/*   Updated: 2025/03/12 17:54:23 by oel-mado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-char	*to_bin(int n)
-{
-	int i;
-	char *bin;
-
-    bin = malloc(9);
-	i = 7;
-	while (i >= 0)
-	{
-		if (n % 2 == 0)
-			bin[i] = '0';
-		else
-			bin[i] = '1';
-		n /= 2;
-		i--;
-	}
-	bin[8] = '\0';
-	return (bin);
-}
-
 void send_char(int c, int pid)
 {
 	int i;
-	char *bits;
 
 	i = 0;
-	bits = to_bit(c);
-	while (i <= 7)
+	while (i < 8)
 	{
-		if (bits[i] == '0')
-			kill(pid, SIGUSR1);
-		else
+		if (c >> i & 1)
+		{
 			kill(pid, SIGUSR2);
+			ft_printf("0");
+		}
+		else
+		{
+			kill(pid, SIGUSR1);
+			ft_printf("1");
+		}
+		i++;
 	}
-	free(bits);
 }
 
 int main(int ac, char **av)
 {
 	int i;
+	int f;
 	int pid;
 
 	i = 0;
+	f = 1;
 	if (ac != 3)
 		exit(0);
 	pid = ft_atoi(av[1]);
-	ft_printf("%d", pid);
 	while (av[2][i])
 	{
+		if (!f)
+			ft_printf(" ");
+		f = 0;
 		send_char(av[2][i], pid);
 		i++;
 	}
+	return (0);
 }
